1f5e8f17a2391dbad98651d19627943b6d12dec9
Aftab <110347957+aftab026@users.noreply.github.com>
2023-03-11 01:40:26 +0530

    Add files via upload

---
+++ T2.c
@@ -0,0 +1,13 @@
+// add integer
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b; 
+    printf("Enter number a,b\n");
+    scanf("%d%d", &a,&b);
+
+    printf("The sum is %d\n", a+b);
+    return 0;
+}
