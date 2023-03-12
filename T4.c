b1b6b46fd06da49f7e1daa82af4129177872c77d
Aftab <110347957+aftab026@users.noreply.github.com>
2023-03-13 00:02:32 +0530

    Add files via upload

---
+++ T4.c
@@ -0,0 +1,18 @@
+// arithmetic operators ( as you possible you use int float etc..)
+
+#include <stdio.h>
+int main()
+{
+    float a, b;
+
+    printf("Emter number a,b\n");
+    scanf("%f%f", &a, &b);
+
+    printf("a + b= %f\n", a + b);
+    printf("a - b= %f\n", a - b);
+    printf("a * b= %f\n", a * b);
+    printf("a / b= %f\n", a / b);
+    //printf("a % b= %f\n", a % b);
+
+    return 0;
+}
