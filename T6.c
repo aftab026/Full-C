6b1ae0a5d2dbb549e84168ab2c718c455e8cf4b5
Aftab <110347957+aftab026@users.noreply.github.com>
2023-03-18 18:35:58 +0530

    Add files via upload

---
+++ T6.c
@@ -0,0 +1,15 @@
+// logical operators
+
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    a = 1;
+    b = 0;
+
+    printf("a && b= %d\n",a &&b );
+    printf("a || b= %d\n",a ||b);
+    printf("a ! b= %d\n", ! b);
+
+    return 0;
+}
