e39adaf98e7d3a7dd1b5e48ecf062d7743dc9c74
Aftab <110347957+aftab026@users.noreply.github.com>
2023-03-19 12:15:56 +0530

    Add files via upload

---
+++ T7.c
@@ -0,0 +1,16 @@
+// bitwise operators
+
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    a = 2;
+    b = 3;
+    printf("a & b= %d\n", a & b);
+    printf("a | b= %d\n", a | b);
+    printf("a ^ b = %d\n", a ^ b);
+    printf("a ~ b= %d\n", ~ a);
+    printf("a << b= %d\n", a << b);
+    printf("a >> b= %d\n", a >> b);
+    return 0;
+}
