8515853c65a311c6d84448ba67b930a8e99c629a
Aftab <110347957+aftab026@users.noreply.github.com>
2023-03-20 14:24:48 +0530

    Add files via upload

---
+++ T8.c
@@ -0,0 +1,14 @@
+// assignment operators
+
+#include<stdio.h>
+int main()
+{
+    int a= 4;
+
+    //printf("a=4 =%d\n", a=4);
+    printf("a+=2 =%d\n", a+=2);
+    printf("a-=2 =%d\n", a-=2);
+    printf("a*=2 =%d\n", a*=2);
+    printf("a/=2 =%d\n", a/=2);
+    return 0;
+}
