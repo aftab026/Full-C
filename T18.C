dfcd2c380f5cbd0590045cc86e64a44773219331
Aftab <110347957+aftab026@users.noreply.github.com>
2023-04-04 21:02:09 +0530

    Add files via upload

---
+++ T18.C
@@ -0,0 +1,17 @@
+// do while loop ic c  example (print counting)
+
+#include<stdio.h>
+int main ()
+{
+    int i=0;
+    do {
+        
+        i=i+1;
+        printf("%d\n", i);
+    }
+    while (i<+100);
+    return 0;
+}
+
+
+
