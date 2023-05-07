29c5ebef25ab410257338913336449c9ba18bc2b
Aftab <110347957+aftab026@users.noreply.github.com>
2023-05-07 15:30:49 +0530

    Add files via upload

---
+++ T21.c
@@ -0,0 +1,15 @@
+// example of do while loop
+
+#include<stdio.h>
+int main()
+{
+int num;
+    printf("Enter a number\n");
+    scanf("%d", &num);
+    do
+    {
+        printf(" Hello World\n");
+    } while (2<1);
+    
+    return 0;
+}
