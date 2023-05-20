2f5eeaa7f0e26724f91ad56d9a8ce73e4512d216
Aftab <110347957+aftab026@users.noreply.github.com>
2023-05-20 12:52:15 +0530

    Add files via upload

---
+++ T24.c
@@ -0,0 +1,14 @@
+// example of for loop
+
+#include<stdio.h>
+int main()
+{
+    int num, i, j=0;
+    for ( i = 0 ; ; )
+    {
+        printf("%d %d\n", i, j);
+        i++; j++;
+    }
+    
+    return 0;
+}
