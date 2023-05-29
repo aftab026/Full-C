4142c6447279e555820cc3bbab7c912b94f16157
Aftab <110347957+aftab026@users.noreply.github.com>
2023-05-29 20:15:27 +0530

    Add files via upload

---
+++ T30.c
@@ -0,0 +1,38 @@
+// example of function
+#include<stdio.h>
+
+int sum(int a, int b);
+int i;
+void printstar(int n)
+
+{
+    for ( i = 0; i < n; i++)
+    {
+        printf("%c", '*');
+    }
+ }
+ /*int takenumber ()
+ {
+     int i;
+     printf("Enter a number\n");
+     scanf("%d", &i);
+     return i;
+ }
+    /*int main()
+    {
+        int a, b, c;
+        a = 9;
+        b = 87;
+         //c = sum (a, b);
+         //printstar(7);
+        c = takenumber();
+         //printf("The sum is %dd \n", c);
+        printf("The number entered is %d \n", c);
+    
+    return 0;
+}*/
+
+//int sum(int a, int b)
+//{
+    //return a + b;
+//}
