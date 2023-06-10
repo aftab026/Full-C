1f45224f5cca2c8e5d2ac72af0e168df806f179b
Aftab <110347957+aftab026@users.noreply.github.com>
2023-06-10 22:11:23 +0530

    Add files via upload

---
+++ T35.c
@@ -0,0 +1,20 @@
+// example for one-dimensional array
+ #include<stdio.h>
+
+ int main()
+ {
+     int marks[10], sum=0;
+     printf("Enter marks of 10 students : \n\n");
+     for (int i = 0; i <=9; i++)
+     {
+         printf("Marks of %d student :", i+1);
+         scanf("%d", &marks[i]);
+         sum += marks[i];
+     }
+
+     int average = sum/10;
+     printf("\nThe average marks of 10 students are %d", average);
+     
+     return 0;
+ }
+ 
