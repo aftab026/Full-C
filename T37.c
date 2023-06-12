f6b49ea732ee515a864c8f5a242a6244425645a9
Aftab <110347957+aftab026@users.noreply.github.com>
2023-06-12 22:56:23 +0530

    Add files via upload

---
+++ T37.c
@@ -0,0 +1,17 @@
+// example of pointer
+#include<stdio.h>
+
+
+int main()
+{
+    //int x=5;
+    //int *a =&x;
+//int a = 5;
+    //printf("%d\n", &x);
+//printf("%d\n", &a);
+    //printf("%d",*a);
+   // printf("%d",a);
+
+   int x,*a;
+    return 0;
+}
