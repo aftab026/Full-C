102e73fb5e95de2903f47fbb491b2de13ee39ad4
Aftab <110347957+aftab026@users.noreply.github.com>
2023-06-23 15:30:41 +0530

    Add files via upload

---
+++ T42.c
@@ -0,0 +1,25 @@
+// example of array and pointer arithmetic...
+#include<stdio.h>
+
+int main()
+{
+    //char a = '3';
+    //char* ptra = &a;
+    //printf("%d\n", ptra);
+    //ptra++;
+    //printf("%d\n", ptra);
+    //printf("%d\n", ptra+2);
+
+    int arr[] = {11,2,3,4,5,6,67};
+    printf("Value at position 3 of the array is %d\n", arr[3]);
+    printf("The address of first element of the array is  %d\n", &arr[0]);
+    printf("The address of first element of the array is  %d\n", arr);
+    printf("The address of second element of the array is  %d\n", &arr[1]);
+    printf("The address of second element of the array is  %d\n", arr + 1);
+
+    printf("The value at address of first element of the array is  %d\n", *(&arr[0]));
+    printf("The value at address of first element of the array is  %d\n", *(arr));
+    printf("The value at address of second element of the array is  %d\n", *(&arr[1]));
+    printf("The value at address of second element of the array is  %d\n", *(arr + 1));
+    return 0;
+}
