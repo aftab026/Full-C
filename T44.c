40a49b6af2b9bd40a0664b4338ba8bf62f0526b6
Aftab <110347957+aftab026@users.noreply.github.com>
2023-06-24 22:52:25 +0530

    Add files via upload

---
+++ T44.c
@@ -0,0 +1,20 @@
+// example of call by reference...
+#include<stdio.h>
+
+void swap(int *x, int *y)
+{
+    int temp;
+    
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+int main()
+{
+    int r=10, v=20;
+    swap(&r, &v);     // passing value of function
+    printf("\nvalue of r: %d",r);
+    printf("\nvalue os v: %d", v);
+    return 0;
+}
