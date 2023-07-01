07dcc7dfc8a13de9f1caad8094ab9d65470ac76c
Aftab <110347957+aftab026@users.noreply.github.com>
2023-07-01 15:01:02 +0530

    Add files via upload

---
+++ T45.c
@@ -0,0 +1,16 @@
+// video code os call by value and reference..
+#include<stdio.h>
+
+void changeValue(int* address)
+{
+    *address = 37565;
+}
+
+int main()
+{
+    int a = 34, b = 56;
+    printf("The value of a now is %d\n", a);
+    changeValue(&a);
+    printf("The value of a now is %d\n", a);
+    return 0;
+}
---
+++ T47.c
@@ -0,0 +1,17 @@
+// example of declaring function with pointer in the parameter....
+#include<stdio.h>
+
+void display(int *ptr) {
+    printf("%d", *ptr);
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4};
+    for ( int i = 0; i < 4; i++)
+    {
+        display(&arr[i]);
+    }
+    
+    return 0;
+}
